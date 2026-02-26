d11b8e181481ef0ab0c7d5af063e794952bf7888
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-26 18:44:00 -0300

    feat: cria save

--- gestao-escolar/src/main/java/com/weg/gestao_escolar/application/service/AlunoService.java
+++ gestao-escolar/src/main/java/com/weg/gestao_escolar/application/service/AlunoService.java
@@ -1,4 +1,39 @@
 package com.weg.gestao_escolar.application.service;
 
+import com.weg.gestao_escolar.application.dto.aluno.AlunoRequestDTO;
+import com.weg.gestao_escolar.application.dto.aluno.AlunoResponseDTO;
+import com.weg.gestao_escolar.application.mapper.AlunoMapper;
+import com.weg.gestao_escolar.domain.model.Aluno;
+import com.weg.gestao_escolar.domain.repository.AlunoRepository;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
+@Service
 public class AlunoService {
+
+    public final AlunoRepository alunoRepository;
+    public final AlunoMapper alunoMapper;
+
+    public AlunoService(AlunoRepository alunoRepository, AlunoMapper alunoMapper){
+        this.alunoRepository = alunoRepository;
+        this.alunoMapper = alunoMapper;
+    }
+
+    public AlunoResponseDTO save(AlunoRequestDTO alunoRequestDTO) {
+        try {
+            Aluno aluno = alunoMapper.requestToEntity(alunoRequestDTO);
+
+            alunoRepository.save(aluno);
+
+            AlunoResponseDTO alunoResponseDTO = alunoMapper.responseToEntity(aluno);
+
+            return alunoResponseDTO;
+
+        } catch (SQLException | RuntimeException e) {
+            throw new RuntimeException("Erro ao salvar Aluno!" + e);
+        }
+    }
+
+
 }
