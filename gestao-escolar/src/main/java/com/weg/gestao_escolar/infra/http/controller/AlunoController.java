946fbb401035663ec85760d22ff39bf717a5ea2e
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-26 19:15:17 -0300

    feat: finaliza controller

--- gestao-escolar/src/main/java/com/weg/gestao_escolar/infra/http/controller/AlunoController.java
+++ gestao-escolar/src/main/java/com/weg/gestao_escolar/infra/http/controller/AlunoController.java
@@ -1,4 +1,44 @@
 package com.weg.gestao_escolar.infra.http.controller;
 
+import com.weg.gestao_escolar.application.dto.aluno.AlunoRequestDTO;
+import com.weg.gestao_escolar.application.dto.aluno.AlunoResponseDTO;
+import com.weg.gestao_escolar.application.service.AlunoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
 public class AlunoController {
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService){
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping
+    public List<AlunoResponseDTO> findAll(){
+        return alunoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public AlunoResponseDTO findById(@PathVariable int id){
+        return alunoService.findById(id);
+    }
+
+    @PostMapping
+    public AlunoResponseDTO save(@RequestBody AlunoRequestDTO alunoRequestDTO){
+        return alunoService.save(alunoRequestDTO);
+    }
+
+    @PutMapping("/{id}")
+    public AlunoResponseDTO update(AlunoRequestDTO alunoRequestDTO){
+        return alunoService.update(alunoRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        alunoService.delete(id);
+    }
 }
