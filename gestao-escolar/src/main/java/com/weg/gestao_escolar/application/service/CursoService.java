bd092ffb862d95c96a6d7088c4218d9337d8f7f3
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-26 21:54:56 -0300

    feat: cria metodo save em curso

--- gestao-escolar/src/main/java/com/weg/gestao_escolar/application/service/CursoService.java
+++ gestao-escolar/src/main/java/com/weg/gestao_escolar/application/service/CursoService.java
@@ -1,4 +1,33 @@
 package com.weg.gestao_escolar.application.service;
 
+import com.weg.gestao_escolar.application.dto.curso.CursoRequestDTO;
+import com.weg.gestao_escolar.application.dto.curso.CursoResponseDTO;
+import com.weg.gestao_escolar.application.mapper.CursoMapper;
+import com.weg.gestao_escolar.domain.model.Curso;
+import com.weg.gestao_escolar.domain.repository.CursoRepository;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
+@Service
 public class CursoService {
+    private final CursoRepository cursoRepository;
+    private final CursoMapper cursoMapper;
+
+    public CursoService(CursoRepository cursoRepository, CursoMapper cursoMapper){
+        this.cursoRepository=cursoRepository;
+        this.cursoMapper=cursoMapper;
+    }
+
+    public CursoResponseDTO save(CursoRequestDTO cursoRequestDTO){
+        try {
+            Curso curso = cursoMapper.requestToEntity(cursoRequestDTO);
+
+            cursoRepository.save(curso);
+
+            return cursoMapper.responseToEntity(curso);
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
 }
