f080205a7bc655143d6a215f7166676bf31a5b5c
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-06 15:44:59 -0300

    feat: finaliza controller

--- gestao-escolar/src/main/java/com/weg/gestao_escolar/infra/web/controller/ProfessorController.java
+++ gestao-escolar/src/main/java/com/weg/gestao_escolar/infra/web/controller/ProfessorController.java
@@ -1,4 +1,44 @@
 package com.weg.gestao_escolar.infra.web.controller;
 
+import com.weg.gestao_escolar.application.dto.professor.ProfessorRequestDTO;
+import com.weg.gestao_escolar.application.dto.professor.ProfessorResponseDTO;
+import com.weg.gestao_escolar.application.service.ProfessorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/professor")
 public class ProfessorController {
+
+    public final ProfessorService professorService;
+
+    public ProfessorController(ProfessorService professorService){
+        this.professorService = professorService;
+    }
+
+    @PostMapping
+    public ProfessorResponseDTO save(@RequestBody ProfessorRequestDTO professorRequestDTO){
+        return professorService.save(professorRequestDTO);
+    }
+
+    @GetMapping
+    public List<ProfessorResponseDTO> findAll(){
+        return professorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProfessorResponseDTO findById(@PathVariable int id){
+        return professorService.findById(id);
+    }
+
+    @PutMapping
+    public void update(@RequestBody ProfessorRequestDTO professorRequestDTO){
+        professorService.update(professorRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        professorService.delete(id);
+    }
 }
