fabc7896002cff56f4eb2449e996dda0125872ec
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-06 16:02:36 -0300

    feat: finaliza metodos service

--- gestao-escolar/src/main/java/com/weg/gestao_escolar/application/service/TurmaService.java
+++ gestao-escolar/src/main/java/com/weg/gestao_escolar/application/service/TurmaService.java
@@ -1,4 +1,76 @@
 package com.weg.gestao_escolar.application.service;
 
+import com.weg.gestao_escolar.application.dto.professor.ProfessorRequestDTO;
+import com.weg.gestao_escolar.application.dto.professor.ProfessorResponseDTO;
+import com.weg.gestao_escolar.application.dto.turma.TurmaRequestDTO;
+import com.weg.gestao_escolar.application.dto.turma.TurmaResponseDTO;
+import com.weg.gestao_escolar.application.mapper.TurmaMapper;
+import com.weg.gestao_escolar.domain.model.Professor;
+import com.weg.gestao_escolar.domain.model.Turma;
+import com.weg.gestao_escolar.domain.repository.TurmaRepository;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
 public class TurmaService {
+
+    private final TurmaRepository turmaRepository;
+    private final TurmaMapper turmaMapper;
+
+    public TurmaService(TurmaRepository turmaRepository, TurmaMapper turmaMapper){
+        this.turmaRepository = turmaRepository;
+        this.turmaMapper = turmaMapper;
+    }
+
+    public TurmaResponseDTO save(TurmaRequestDTO professorRequestDTO){
+        try {
+            Turma professor = turmaMapper.requestToEntity(professorRequestDTO);
+
+            turmaRepository.save(professor);
+
+            return turmaMapper.responseToEntity(professor);
+        } catch (SQLException | RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public TurmaResponseDTO findById(int id){
+        try {
+            Turma professor = turmaRepository.findById(id);
+
+            return turmaMapper.responseToEntity(professor);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<TurmaResponseDTO> findAll(){
+        try {
+            return turmaRepository.findAll().stream()
+                    .map(turmaMapper::responseToEntity)
+                    .toList();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(TurmaRequestDTO professorRequestDTO){
+        try {
+            Turma professor = turmaMapper.requestToEntity(professorRequestDTO);
+
+            turmaRepository.update(professor);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(int id){
+        try {
+            turmaRepository.delete(id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
