a28732c212305910ba8b08c419a6b057fca1ef7c
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-06 15:21:14 -0300

    feat: finaliza NotaService.java

--- gestao-escolar/src/main/java/com/weg/gestao_escolar/application/service/NotaService.java
+++ gestao-escolar/src/main/java/com/weg/gestao_escolar/application/service/NotaService.java
@@ -1,4 +1,74 @@
 package com.weg.gestao_escolar.application.service;
 
+import com.weg.gestao_escolar.application.dto.nota.NotaRequestDTO;
+import com.weg.gestao_escolar.application.dto.nota.NotaResponseDTO;
+import com.weg.gestao_escolar.application.mapper.NotaMapper;
+import com.weg.gestao_escolar.domain.model.Nota;
+import com.weg.gestao_escolar.domain.repository.NotaRepository;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
 public class NotaService {
+
+    private final NotaRepository notaRepository;
+    private final NotaMapper notaMapper;
+
+    public NotaService(NotaRepository notaRepository, NotaMapper notaMapper){
+        this.notaRepository = notaRepository;
+        this.notaMapper= notaMapper;
+    }
+
+    public NotaResponseDTO save(NotaRequestDTO notaRequestDTO){
+        try {
+            Nota nota = notaMapper.requestToEntity(notaRequestDTO);
+
+            notaRepository.save(nota);
+
+            return notaMapper.responseToEntity(nota);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public NotaResponseDTO findById(int id){
+        try {
+            Nota nota = notaRepository.findById(id);
+
+            return notaMapper.responseToEntity(nota);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<NotaResponseDTO> findAll(){
+        try {
+            return notaRepository.findAll().stream()
+                    .map(notaMapper::responseToEntity)
+                    .toList();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(NotaRequestDTO notaRequestDTO){
+        try {
+            Nota nota = notaMapper.requestToEntity(notaRequestDTO);
+
+            notaRepository.update(nota);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(int id){
+        try {
+            notaRepository.delete(id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
